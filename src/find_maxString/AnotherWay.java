8c49e42a770bb1dc6799c5c1dc7cc52cc669ab2c
TranAnhHao23 <Trananhhaonuce@gmail.com>
2021-12-21 09:26:43 +0700

    Add file

---
+++ src/find_maxString/AnotherWay.java
@@ -0,0 +1,33 @@
+package find_maxString;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AnotherWay {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào chuỗi ký tự: ");
+        String str = sc.nextLine();
+        findMaxString(str);
+    }
+
+    public static void findMaxString(String str) {
+        char[] ch = str.toCharArray();
+        ArrayList<Character> characterArrayList = new ArrayList<>();
+        characterArrayList.add(ch[0]);
+        for (int i = 0; i < ch.length; i++) {
+            for (int j = i + 1; j < ch.length; j++) {
+                char temp = characterArrayList.get(characterArrayList.size() - 1);
+                if (ch[j] > characterArrayList.get(characterArrayList.size() - 1)) {
+//                    System.out.println(characterArrayList.get(characterArrayList.size() - 1));
+                    characterArrayList.add(ch[j]);
+                    break;
+                }
+            }
+        }
+        for (char cha : characterArrayList) {
+            System.out.print(cha);
+        }
+
+    }
+}
